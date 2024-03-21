771c0627011f74a0b8bb907383868779232cfc49
DanHozan123 <danhozan@gmail.com>
2024-03-21 11:34:53 +0200

    Initial Commit

---
+++ eBay2.0/eBay2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  eBay2_0App.swift
+//  eBay2.0
+//
+//  Created by Dan Hozan on 21.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct eBay2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
